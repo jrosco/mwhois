25f3eb39b5b9c03302d5a73d8234caef85ad36dd
jrosco <joel_c@zoho.com>
2014-01-29 20:16:31 +1000

    -

--- testing/wxdemo.py
+++ testing/wxdemo.py
@@ -1,22 +1,99 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# simplemenu.py
+import wx, time
+# an observable calls callback functions when the data has changed
+#o = Observable()
+#def func(data):
+# print "hello", data
+#o.addCallback(func)
+#o.set(1)
+# --| "hello", 1
+class Observable:
+    def __init__(self, initialValue=None):
+        self.data = initialValue
+        self.callbacks = {}
 
-import wx
+    def addCallback(self, func):
+        self.callbacks[func] = 1
 
-class SimpleMenu(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(250, 150))
+    def delCallback(self, func):
+        del self.callback[func]
 
-        menubar = wx.MenuBar()
-        file = wx.Menu()
-        file.Append(-1, 'Quit', 'Quit application')
-        menubar.Append(file, '&File')
-        self.SetMenuBar(menubar)
+    def _docallbacks(self):
+        for func in self.callbacks:
+            func(self.data)
 
-        self.Centre()
-        self.Show(True)
+    def set(self, data):
+        self.data = data
+        self._docallbacks()
 
-app = wx.App()
-SimpleMenu(None, -1, 'simple menu example')
+    def get(self):
+        return self.data
+
+    def unset(self):
+        self.data = None
+
+class Model:
+    def __init__(self):
+        self.myMoney = Observable(0)
+
+    def addMoney(self, value):
+        self.myMoney.set(self.myMoney.get() + value)
+
+    def removeMoney(self, value):
+        self.myMoney.set(self.myMoney.get() - value)
+
+
+class View(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, title="Main View")
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        text = wx.StaticText(self, label="My Money")
+        ctrl = wx.TextCtrl(self)
+        sizer.Add(text, 0, wx.EXPAND | wx.ALL)
+        sizer.Add(ctrl, 0, wx.EXPAND | wx.ALL)
+        ctrl.SetEditable(False)
+        self.SetSizer(sizer)
+        self.moneyCtrl = ctrl
+
+    def SetMoney(self, money):
+        self.moneyCtrl.SetValue(str(money))
+
+
+class ChangerWidget(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, title="Main View")
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.add = wx.Button(self, label="Add Money")
+        self.remove = wx.Button(self, label="Remove Money")
+        sizer.Add(self.add, 0, wx.EXPAND | wx.ALL)
+        sizer.Add(self.remove, 0, wx.EXPAND | wx.ALL)
+        self.SetSizer(sizer)
+
+class Controller:
+    def __init__(self, app):
+        self.model = Model()
+        self.view1 = View(None)
+        self.view2 = ChangerWidget(self.view1)
+        self.MoneyChanged(self.model.myMoney.get())
+        self.view2.add.Bind(wx.EVT_BUTTON, self.AddMoney)
+        self.view2.remove.Bind(wx.EVT_BUTTON, self.RemoveMoney)
+        self.model.myMoney.addCallback(self.MoneyChanged)
+        self.view1.Show()
+        self.view2.Show()
+
+    def AddMoney(self, evt):
+        
+        for x in range(0,10):
+            time.sleep(0.2)
+            self.model.addMoney(10)
+
+    def RemoveMoney(self, evt):
+        self.model.removeMoney(10)
+
+    def MoneyChanged(self, money):
+        self.view1.SetMoney(money)
+
+app = wx.App(False)
+controller = Controller(app)
 app.MainLoop()
