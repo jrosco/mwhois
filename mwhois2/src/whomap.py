ff3eeac7873357eaed02f54da546c2c22adcd80f
jrosco <joel_c@zoho.com>
2014-02-05 15:36:38 +1100

    final stages of whois mapping

--- mwhois2/src/whomap.py
+++ mwhois2/src/whomap.py
@@ -32,7 +32,7 @@ class WhoisServerMap(object):
                         'coop': ['whois.nic.coop','', ''], \
                         'cx': ['whois.nic.cx','', 'No Object Found' ,''], \
                         'cz': ['whois.nic.cz','', 'No data found' ,''], \
-                        'de': ['whois.denic.de','', 'Status: [free' ,''], \
+                        'de': ['whois.denic.de','', 'Status: free' ,''], \
                         'dk': ['whois.dk-hostmaster.dk','', 'No entries found' ,''], \
                         'edu': ['whois.educause.edu','', 'No Match' ,''], \
                         'ee': ['whois.eenet.ee','', ''], \
@@ -109,12 +109,108 @@ class WhoisServerMap(object):
                         'yt': ['whois.nic.yt','', 'No entries found' ,''] }
         
         
-        self.all_info_map = {'org' : ['Creation Date:\s*(.+)','Expiry Date:\s*(.+)','Updated Date:\s*(.+)','Registrant\s*(.+)','Name Server:\s*(.+)','n/a'], \
-                             'com' : ['Creation Date:\s*(.+)','N/A','Updated Date:\s*(.+)','Registrant\s*(.+)','Name Server:\s*(.+)', 'Domain Status' ], \
-                             'net' : ['Creation Date:\s*(.+)','N/A','Updated Date:\s*(.+)','Registrant\s*(.+)','Name Server:\s*(.+)', 'Domain Status']}
+        self.all_info_map = { 'ac' : ['N/A', 'Expiry :\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'aero' : ['N/A', 'Expires On:\s*(.+)', 'Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'af' : ['Creation Date\s*(.+)', 'Registry Expiry Date\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'ag' : ['Created On\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'am' : ['Registered:\s*(.+)', 'Expires:\s*(.+)', 'Last modified:\s*(.+)', 'Registrant:\s*(.+)', 'N/A'], \
+                         'as' : ['Created:\s*(.+)', 'Expires:\s*(.+)', 'N/A', 'Registrant:\s*(.+)', 'Name Servers:\s*(.+)'], \
+                         'at' : ['N/A', 'N/A', 'N/A', 'registrant:\s*(.+)', 'N/A'], \
+                         'au' : ['N/A', 'N/A', 'N/A', 'Registrant\s*(.+)', 'N/A'], \
+                         'be' : ['N/A', 'N/A', 'N/A', 'Registrant:\s*(.+)', 'Nameservers:\s*(.+)'], \
+                         'biz' : ['Created\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'bj' : ['N/A', 'N/A', 'Last Updated:\s*(.+)', 'N/A', 'N/A'], \
+                         'br' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'bz' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ca' : ['Creation Date\s*(.+)', 'Expiry date\s*(.+)', 'Updated date:\s*(.+)', 'Registrant:\s*(.+)', 'Name servers:\s*(.+)'], \
+                         'cat' : ['Created On\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server\s*(.+)'], \
+                         'cc' : ['Creation Date\s*(.+)', 'N/A', 'N/A', 'N/A', 'Name Server:\s*(.+)'], \
+                         'cd' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ch' : ['N/A', 'N/A', 'N/A', 'N/A', 'Name servers:\s*(.+)'], \
+                         'ci' : ['N/A', 'Expiration date:\s*(.+)', 'N/A', 'N/A', 'Nameserver:\s*(.+)'], \
+                         'cl' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'cn' : ['N/A', 'Expiration Date:\s*(.+)', 'N/A', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'com' : ['N/A', ' Expiration Date:\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'coop' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'cx' : ['Creation Date\s*(.+)', 'Registry Expiry Date\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'cz' : ['created:\s*(.+)', 'N/A', 'N/A', 'registrant:\s*(.+)', 'N/A'], \
+                         'de' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'dk' : ['N/A', 'Expires:\s*(.+)', 'N/A', 'N/A', 'Nameservers\s*(.+)'], \
+                         'edu' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ee' : ['created:\s*(.+)', 'N/A', 'N/A', 'registrant:\s*(.+)', 'N/A'], \
+                         'eu' : ['N/A', 'N/A', 'N/A', 'N/A', 'Name servers:\s*(.+)'], \
+                         'fi' : ['created:\s*(.+)', 'expires:\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'fr' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'gf' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'gg' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'gov' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'gr' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'hk' : ['N/A', 'Expiry Date:\s*(.+)', 'N/A', 'N/A', 'Name Server:\s*(.+)'], \
+                         'hn' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'hu' : ['record created\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ie' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'il' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'in' : ['Created On\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'info' : ['Created On\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'int' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'io' : ['N/A', 'Expires :\s*(.+)', 'Last Updated :\s*(.+)', 'N/A', 'N/A'], \
+                         'is' : ['created:\s*(.+)', 'expires:\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'it' : ['Created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'Nameservers\s*(.+)'], \
+                         'jp' : ['Created on\s*(.+)', 'Expires on\s*(.+)', 'Last Updated\s*(.+)', 'N/A', 'Name Server\s*(.+)'], \
+                         'ke' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'kr' : ['N/A', 'Expiration Date             :\s*(.+)', 'Last Updated Date           :\s*(.+)', 'Registrant\s*(.+)', 'Name Server\s*(.+)'], \
+                         'kz' : ['Domain created\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'li' : ['N/A', 'N/A', 'N/A', 'N/A', 'Name servers:\s*(.+)'], \
+                         'lt' : ['N/A', 'N/A', 'N/A', 'N/A', 'Nameserver:\s*(.+)'], \
+                         'lu' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'lv' : ['N/A', 'N/A', 'Updated:\s*(.+)', 'N/A', 'N/A'], \
+                         'mg' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'mil' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'mn' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ms' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'museum' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'mx' : ['Created On\s*(.+)', 'Expiration Date:   \s*(.+)', 'Last Updated On:   \s*(.+)', 'N/A', 'Name Servers:\s*(.+)'], \
+                         'my' : ['Record Created\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'na' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'name' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'net' : ['Creation Date\s*(.+)', 'N/A', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'nl' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'no' : ['N/A', 'N/A', 'Last updated:\s*(.+)', 'N/A', 'N/A'], \
+                         'nu' : ['N/A', 'expires:\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'nz' : ['domain_date registered:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'org' : ['Creation Date\s*(.+)', 'Expiry Date:\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'pl' : ['option created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'pm' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'pr' : ['N/A', 'expiration date:       \s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'pro' : ['created:\s*(.+)', 'Expiration Date:\s*(.+)', 'Last Updated On:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         're' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ro' : ['Registered On:\s*(.+)', 'N/A', 'N/A', 'N/A', 'Nameserver:\s*(.+)'], \
+                         'ru' : ['created:\s*(.+)', 'N/A', 'Last updated on\s*(.+)', 'N/A', 'N/A'], \
+                         'se' : ['created:\s*(.+)', 'expires:\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'sg' : ['Creation Date\s*(.+)', 'Expiration Date:\s*(.+)', 'N/A', 'N/A', 'Name Servers:\s*(.+)'], \
+                         'sh' : ['N/A', 'Expiry :\s*(.+)', 'N/A', 'N/A', 'N/A'], \
+                         'si' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'nameserver:\s*(.+)'], \
+                         'st' : ['created-date:\s*(.+)', 'expiration-date:\s*(.+)', 'updated-date:\s*(.+)', 'registrant-\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'tf' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'tj' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'tk' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'tl' : ['Creation Date\s*(.+)', 'Expiry Date:\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'tm' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'to' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'tr' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'tv' : ['Creation Date\s*(.+)', 'N/A', 'Updated Date:\s*(.+)', 'N/A', 'Name Server:\s*(.+)'], \
+                         'tw' : ['Record created on\s*(.+)', 'expires on\s*(.+)', 'N/A', 'Registrant\s*(.+)', 'N/A'], \
+                         'ua' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ug' : ['Registered:\s*(.+)', 'Expiry:             \s*(.+)', 'Updated:\s*(.+)', 'N/A', 'Nameserver:\s*(.+)'], \
+                         'uk' : ['Registered on:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'us' : ['Last Transferred Date:\s*(.+)', 'Expiration Date:\s*(.+)', 'Domain Last Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'uz' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         've' : ['N/A', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'wf' : ['created:\s*(.+)', 'N/A', 'N/A', 'N/A', 'N/A'], \
+                         'ws' : ['Creation Date\s*(.+)', 'Expiration Date:\s*(.+)', 'Updated Date:\s*(.+)', 'Registrant\s*(.+)', 'Name Server:\s*(.+)'], \
+                         'yt' : ['created:\s*(.+)', 'anniversary:\s*(.+)', 'last-update:\s*(.+)', 'N/A', 'N/A' ] }
+                         
 
-    
-        
         
 #         self.server_map =            {'de' : 'whois.denic.de', \
 #                                         'uk' : 'whois.nic.uk', \
