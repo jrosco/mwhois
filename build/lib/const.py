3f527c300b5bef4a062fe03297ac501ed8e11c1c
jrosco <joel_c@zoho.com>
2014-02-23 16:59:39 +1100

    removed old files

--- build/lib/const.py
+++
@@ -1,7 +0,0 @@
-DOMAIN_FOUND = "Domain Available"
-DOMAIN_FOUND_ADV = "Domain Found but could be Parked, a Dead Site or a Redirected Domain" 
-DOMAIN_DEAD = 0
-DOMAIN_ALIVE = 1
-SINGLE_TYPE = 1
-BASIC_TYPE = 2
-ADV_TYPE = 3
--- build/lib/mwhois/exception.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python 
-
-class WhoException():
-    
-    def __init__(self, e):
-        print(e)
