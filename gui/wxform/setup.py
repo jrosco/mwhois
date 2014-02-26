8369d5079fed85cac920b7c7ceb2049bcd79e391
jrosco <joel_c@zoho.com>
2014-02-26 17:27:57 +1100

    updated/added setup files

--- gui/wxform/setup.py
+++ gui/wxform/setup.py
@@ -1,22 +1,18 @@
-import sys
-from cx_Freeze import setup, Executable
+from distutils.core import setup
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {}
+version = '0.1.0b'
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-target_name = 'mwhois'
-#targetDir = 'build'
+long_description = '''\
+Whois cleint capable of finding multiple available domains via a file or list type. 
+It's a good way to search for domains that are available to buy.'''
 
-if sys.platform == "win32":
-    base = "Win32GUI"
-
-setup(  name = "mwhois",
-        version='1.0b',
-        author='jrosco',
-        description = "Multi Whois Client",
-        license='GPL',
-        options = {"build_exe": build_exe_options},
-        executables = [Executable("__init__.py", base=base, targetName=target_name)])
+setup( name='mwhois-cleint',
+       version=version,
+       long_description=long_description,
+       author='Joel Cumberland',
+       author_email='joel_c@zoho.com',
+       license='GPL',
+       package_dir={'mwhois-client': 'src'},
+       packages=['mwhois-client'],
+       #py_modules=['whois', 'whoconnect', 'whomap', 'whosearch','const']
+      )
