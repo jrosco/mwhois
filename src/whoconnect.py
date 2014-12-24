3dc4bc94bacb12f5e648b418c123aec46410a343
jrosco <joel_c@zoho.com>
2014-12-24 13:35:33 +1100

    added reset default proxy support in finally statements

--- src/whoconnect.py
+++ src/whoconnect.py
@@ -7,13 +7,13 @@ from exception import WhoException
 
 
 class WhoisServerConnection():
-    
+
     def __init__(self, whoinfo):
 
         self.logger = logging.getLogger(__name__)
 
         self.logger.debug('constructor: __init__()')
-        
+
         self.whoinfo = whoinfo
         self.sleep = 0
         self.no_of_attempts = 0
@@ -24,7 +24,7 @@ class WhoisServerConnection():
         self.proxy_type = None
         self.proxy_user = None
         self.proxy_password = None
-        
+
     def connection(self):
 
         self.logger.debug('called connection(%s)' % self.whoinfo.whoisserver)
@@ -40,63 +40,69 @@ class WhoisServerConnection():
                 raise WhoException(e)
 
         if str(self.whoinfo.whoisserver):
-            
+
             self.logger.debug('sleep for %f', self.sleep)
             time.sleep(self.sleep)
-            
-            try:
-                self.logger.debug('socket: creating socket: fingers crossed ')
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 
-                sock.settimeout(self.timeout)
+            try:
 
-                self.logger.debug('socket: %s', sock)
-            
-            except socket.error as e:
-                #self.logger.error('socket: open socket error %s', e)
-                raise WhoException(e)
-                sock.close()
-                
-            try:
-                self.logger.debug('connecting to server: %s', self.whoinfo.whoisserver)
-                str(self.whoinfo.whoisserver)
-                sock.connect((self.whoinfo.whoisserver, 43))
-            
-            except socket.error as e:
-                #self.logger.error('socket: connect socket error %s', e, exc_info=True)
-                raise WhoException(e)
-                sock.close()
-                
-            try:
-                self.logger.debug('socket: now sending data to server (data:%s)', self.whoinfo.domain)
-                sock.send(self.whoinfo.domain + '\r\n')
-            
-            except socket.error as e:
-                #self.logger.error('socket: send error %s', e, exc_info=True)
-                raise WhoException(e)
-                sock.close()
-                    
-            try:
-                
-                self.logger.debug('socket: lets clear self.response so we get fresh data')
-                self.whoinfo.response = ''
-                
-                while True:
-                    data = sock.recv(4096)
-                    self.logger.debug('socket: receiving data')
-                    self.whoinfo.response += data
-                    if data == '':
-                        self.logger.debug('socket: end of data sequence')
-                        break     
-                
-                self.logger.debug('socket: closing socket object %s', sock)
-                sock.close()
-                """ Reset the proxy """
-                socks.socksocket.default_proxy = None
-            
-            except Exception as e:
-                    raise WhoException(e)
+                try:
+                    self.logger.debug('socket: creating socket: fingers crossed ')
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+                    self.logger.debug('socket: %s', sock)
+
+                    self.logger.debug('socket time value: %s', self.timeout)
+                    sock.settimeout(self.timeout)
+
+                finally:
+                    """ Reset the proxy """
+                    try:socks.socksocket.default_proxy = None
+                    except:pass
+
+                try:
+                    self.logger.debug('connecting to server: %s', self.whoinfo.whoisserver)
+                    str(self.whoinfo.whoisserver)
+                    sock.connect((self.whoinfo.whoisserver, 43))
+
+                finally:
+                    """ Reset the proxy """
+                    try:socks.socksocket.default_proxy = None
+                    except:pass
+
+                try:
+                    self.logger.debug('socket: now sending data to server (data:%s)', self.whoinfo.domain)
+                    sock.send(self.whoinfo.domain + '\r\n')
+
+                finally:
+                    """ Reset the proxy """
+                    try:socks.socksocket.default_proxy = None
+                    except:pass
+
+                try:
+
+                    self.logger.debug('socket: lets clear self.response so we get fresh data')
+                    self.whoinfo.response = ''
+
+                    while True:
+                        data = sock.recv(4096)
+                        self.logger.debug('socket: receiving data')
+                        self.whoinfo.response += data
+                        if data == '':
+                            self.logger.debug('socket: end of data sequence')
+                            break
+
+                    self.logger.debug('socket: closing socket object %s', sock)
                     sock.close()
+
+                finally:
+                    """ Reset the proxy """
+                    try:socks.socksocket.default_proxy = None
+                    except:pass
+
+            except socket.error as e:
+                sock.close()
+                raise WhoException(e)
         else:
                 self.logger.debug('no whois server is set :( ')
 
